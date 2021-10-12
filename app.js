37e7c4edec0c11c0d7dd32027a70482422a73a44
phare <fairphare@gmail.com>
2021-10-13 03:00:56 +0700

    feat: add cors to app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes');
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 // API route
 app.use('/', indexRouter);
