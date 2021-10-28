32b2e68cf0e429d0977be03f0b944c6cfbf180f8
mmkanta <47110972+mmkanta@users.noreply.github.com>
2021-10-28 14:47:10 +0700

    add upload file

---
+++ routes/example.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../middlewares/uploadFile')
+const path = require('path')
+
+// upload file
+router.post('/', upload.single('file'), async (req, res) => {
+    console.log(req.file)
+    return res.status(200).send('success')
+});
+
+// get file
+router.get('/:filename', async (req, res) => {
+    const uploadPath = path.join(__dirname, '../resources/uploads/', req.params.filename)
+    return res.status(200).sendFile(uploadPath)
+})
+
+module.exports = router;
