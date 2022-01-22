db3a01203d20bfe35b6c365869d3ba6f207ba2de
mmkanta <47110972+mmkanta@users.noreply.github.com>
2022-01-22 19:43:57 +0700

    add save to pacs

--- routes/pacs.js
+++ routes/pacs.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const pacsController = require('../controllers/pacsController')
+const verification = require('../middlewares/verification')
 
 // get all dicom rows by patient's HN
 router.get('/HN/:HN', pacsController.getAllByHN)
 
 // get patient's data from PACS
 router.get('/HN/:HN/info', pacsController.getInfoByHN);
+
+// save back to PACS
+router.post(
+    '/save',
+    tokenValidation,
+    verification.reportVerification,
+    pacsController.saveToPACS
+)
 
 module.exports = router;
