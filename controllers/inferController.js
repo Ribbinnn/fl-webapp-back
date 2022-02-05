07b7cf987a3891b96aaa65cb6ea25ab4b6540319
mmkanta <47110972+mmkanta@users.noreply.github.com>
2022-02-05 16:32:32 +0700

    Update inferController.js

--- controllers/inferController.js
+++ controllers/inferController.js
@@ -160,6 +160,8 @@ const inferResult = async (req, res) => {
                             prediction.push({
                                 finding: modelResult["Finding"][i],
                                 confidence: modelResult["Confidence"][i],
+                                threshold: modelResult["Threshold"][i],
+                                isPositive: modelResult["isPositive"][i],
                                 selected: false
                             })
                         }
