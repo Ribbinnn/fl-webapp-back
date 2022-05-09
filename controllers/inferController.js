ad6785742c5350ef5a8f4a69556603ca293328f3
mmkanta <47110972+mmkanta@users.noreply.github.com>
2022-05-10 01:13:41 +0700

    fix upload record

--- controllers/inferController.js
+++ controllers/inferController.js
@@ -59,10 +59,11 @@ const inferResult = async (req, res) => {
     // check required fields
     for (const requirement of requirements) {
         const fieldName = requirement.name
-        if (req.body.record[fieldName] === undefined && !requirement.required) {
+        if ((req.body.record[fieldName] === undefined || req.body.record[fieldName] === null) && !requirement.required) {
             req.body.record[fieldName] = null
             continue
         }
+        console.log(fieldName, req.body.record[fieldName])
         if (!req.body.record[fieldName] && requirement.required)
             return res.status(400).json({ success: false, message: `Invalid record input: "${fieldName}" is required` })
         // check fields' type
